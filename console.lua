aa9780336ad85e568a2a1967b7a6db1aaee9193c
Invision <mariomario13245@gmail.com>
2019-04-20 17:23:49 -0300

    oops forgot license thingy

--- console.lua
+++ console.lua
@@ -1,6 +1,22 @@
--- https://github.com/Luigimaster1/cyf-console/
--- written by ally
--- please dont steal and claim as your own
+--[[
+    console.lua, an interactive console library for Create Your Frisk
+    https://github.com/Luigimaster1/cyf-console/
+
+    Copyright (C) 2019  Alexia Tilde
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]]--
 
 local self = {}
 
